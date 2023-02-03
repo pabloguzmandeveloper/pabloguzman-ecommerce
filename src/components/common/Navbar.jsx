5f9134611c5c23a73848ece1a3198def655fa3ed
paDaGuDev <pguzmanit@gmail.com>
2023-02-02 21:20:32 -0300

    corrections folder and files

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -2,31 +2,6 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default function Navbar() {
-    return(
-        <>
-            <div class="bars__menu">
-                <span class="line1__bars-menu"></span>
-                <span class="line2__bars-menu"></span>
-                <span class="line3__bars-menu"></span>
-            </div>
-        </>
-    )
-};
-//----------------------------------------------
-
-
-document.querySelector(".bars__menu").addEventListener("click", animateBars);
-
-var line1__bars = document.querySelector(".line1__bars-menu");
-var line2__bars = document.querySelector(".line2__bars-menu");
-var line3__bars = document.querySelector(".line3__bars-menu");
-
-function animateBars(){
-    line1__bars.classList.toggle("activeline1__bars-menu");
-    line2__bars.classList.toggle("activeline2__bars-menu");
-    line3__bars.classList.toggle("activeline3__bars-menu");
-}
-
             <div className='navbar__menu'>
                 <ul>
                     <li className='navbar__menu--buy'></li>
@@ -35,3 +10,31 @@ function animateBars(){
                     <li className='navbar__menu--aboutUs'></li>
                 </ul>
             </div>
+};
+//----------------------------------------------
+
+//     document.querySelector(".bars__menu").addEventListener({click}, animateBars);
+
+// var line1__bars = document.querySelector(".line1__bars-menu");
+//     var line2__bars = document.querySelector(".line2__bars-menu");
+//     var line3__bars = document.querySelector(".line3__bars-menu");
+
+//     function animateBars(){
+//         line1__bars.classList.toggle("activeline1__bars-menu");
+//         line2__bars.classList.toggle("activeline2__bars-menu");
+//         line3__bars.classList.toggle("activeline3__bars-menu");
+//     }
+//     return(
+//         <>
+//             <div class="bars__menu">
+//                 <span class="line1__bars-menu"></span>
+//                 <span class="line2__bars-menu"></span>
+//                 <span class="line3__bars-menu"></span>
+//             </div>
+//         </>  
+
+    
+
+//     )
+
+
